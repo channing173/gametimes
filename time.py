2635f1bef038678ae9caf7991ae1aaee810182d6
willi7cg <willi7cg@dukes.jmu.edu>
2018-02-14 16:36:51 -0500

    Added calculator for Alex

--- time.py
+++ time.py
@@ -23,23 +23,36 @@ def hours(time1, time2):
 
     print "%d hrs and %d mins to games" % (dishr, dismin)
     
-def time_to_games(time):
-    atime = pactime.strftime('%I%M')
-    hours(str(atime), time)    
+def time_to_games(person, time):
+    if person == "Channing":
+        atime = pactime.strftime('%I%M')
+        hours(str(atime), time)
+
+    elif person == "Alex":
+        ctime = eastime.strftime('%I%M')
+        hours(str(ctime), time)
 
 def printer():
     print "Channing's Time: ", eastime.strftime('%I:%M %p')
 
     print "Alex's Time: ", pactime.strftime('%I:%M %p')
 
+    person = str(raw_input("Who are we calculating for?\n"))
+    
+    while person != "Alex" and person != "Channing":
+        person = str(raw_input("Who are we calculating for?\n Alex or Channing?"))
+
     time = str(raw_input("When are games? \n"))
 
-    if len(time) != 4:
+    while len(time) != 4:
         time = str(raw_input("I need 4 digits, when are games? \n"))
+        
+    if person == "Channing":
+        print "Games will be at %s" % (int(time) + 300)
+    elif person == "Alex":
+        print "Games will be at %s" % (int(time) - 300)
 
-    print "Games will be at %s" % (int(time) + 300)
-
-    time_to_games(time)
+    time_to_games(person, time)
 
 if __name__ == '__main__':
     printer()
